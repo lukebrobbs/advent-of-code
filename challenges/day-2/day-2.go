3605ef9dc9fff9e0102a22b1c6ff703069f03adc
Luke Brobbs <lukemichaelvocalist@gmail.com>
2023-12-02 13:46:59 +0000

    day 2 part 2

--- challenges/day-2/day-2.go
+++ challenges/day-2/day-2.go
@@ -18,12 +18,18 @@ func Day2(input string, part2 bool) int {
 	values := strings.Split(input, "\n")
 	cubes := getCubes()
 	validGames := []int{}
-gameLoop:
+	power := []int{}
+
 	for _, game := range values {
 		gr := strings.Split(game, ":")
 		gameNumber, _ := strconv.Atoi(strings.Split(gr[0], " ")[1])
 		games := strings.Split(gr[1], ";")
-
+		highestColors := map[string]int{
+			"red":   0,
+			"green": 0,
+			"blue":  0,
+		}
+		valid := true
 		for _, round := range games {
 			picks := strings.Split(round, ",")
 			for _, pick := range picks {
@@ -31,18 +37,32 @@ gameLoop:
 				total := strings.Split(pick, " ")
 				amount, _ := strconv.Atoi(total[0])
 				color := total[1]
+				if highestColors[color] < amount {
+					highestColors[color] = amount
+				}
 				if cubes[color] < amount {
-					continue gameLoop
+					valid = false
 				}
 			}
 		}
-		validGames = append(validGames, gameNumber)
+		if valid {
+			validGames = append(validGames, gameNumber)
+		}
+		multiplied := highestColors["red"] * highestColors["green"] * highestColors["blue"]
 
+		power = append(power, multiplied)
 	}
 	var total int
 
-	for _, v := range validGames {
-		total += v
+	if part2 {
+		for _, v := range power {
+			total += v
+		}
+	} else {
+		for _, v := range validGames {
+			total += v
+		}
 	}
+
 	return total
 }
--- challenges/day-2/day-2_test.go
+++ challenges/day-2/day-2_test.go
@@ -13,7 +13,7 @@ func TestDay2(t *testing.T) {
 		expected int
 	}{
 		"part 1": {input: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", part2: false, expected: 8},
-		// "part 2": {input: "", part2: true, expected: 4},
+		"part 2": {input: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", part2: true, expected: 2286},
 	}
 
 	for name, tc := range tests {
