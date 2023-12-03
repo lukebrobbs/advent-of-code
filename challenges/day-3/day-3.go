a916a748aea9c29577e07d3e30768ccc90f243be
Luke Brobbs <lukemichaelvocalist@gmail.com>
2023-12-03 08:25:06 +0000

    day 3

---
+++ challenges/day-3/day-3.go
@@ -0,0 +1,60 @@
+package day3
+
+import (
+	"image"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func Day3(input string, part2 bool) int {
+
+	grid := map[image.Point]rune{}
+	for y, s := range strings.Fields(string(input)) {
+		for x, r := range s {
+			if r != '.' && !unicode.IsDigit(r) {
+				grid[image.Point{x, y}] = r
+			}
+		}
+	}
+
+	parts := map[image.Point][]int{}
+	for y, s := range strings.Fields(string(input)) {
+		for _, m := range regexp.MustCompile(`\d+`).FindAllStringIndex(s, -1) {
+			bounds := map[image.Point]struct{}{}
+			for x := m[0]; x < m[1]; x++ {
+				for _, d := range []image.Point{
+					{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+					{0, 1}, {1, -1}, {1, 0}, {1, 1},
+				} {
+					bounds[image.Point{x, y}.Add(d)] = struct{}{}
+				}
+			}
+
+			n, _ := strconv.Atoi(s[m[0]:m[1]])
+			for p := range bounds {
+				if _, ok := grid[p]; ok {
+					parts[p] = append(parts[p], n)
+				}
+			}
+		}
+	}
+
+	part1, part2Ret := 0, 0
+	for p, ns := range parts {
+		prod := 1
+		for _, n := range ns {
+			part1 += n
+			prod *= n
+		}
+		if grid[p] == '*' && len(ns) == 2 {
+			part2Ret += prod
+		}
+	}
+	if part2 {
+		return part2Ret
+	}
+	return part1
+
+}
---
+++ challenges/day-3/day-3_test.go
@@ -0,0 +1,61 @@
+package day3
+
+import (
+	"fmt"
+	"image"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"testing"
+	"unicode"
+)
+
+func TestDay3(t *testing.T) {
+	input, _ := os.ReadFile("input.txt")
+
+	grid := map[image.Point]rune{}
+	for y, s := range strings.Fields(string(input)) {
+		for x, r := range s {
+			if r != '.' && !unicode.IsDigit(r) {
+				grid[image.Point{x, y}] = r
+			}
+		}
+	}
+
+	parts := map[image.Point][]int{}
+	for y, s := range strings.Fields(string(input)) {
+		for _, m := range regexp.MustCompile(`\d+`).FindAllStringIndex(s, -1) {
+			bounds := map[image.Point]struct{}{}
+			for x := m[0]; x < m[1]; x++ {
+				for _, d := range []image.Point{
+					{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+					{0, 1}, {1, -1}, {1, 0}, {1, 1},
+				} {
+					bounds[image.Point{x, y}.Add(d)] = struct{}{}
+				}
+			}
+
+			n, _ := strconv.Atoi(s[m[0]:m[1]])
+			for p := range bounds {
+				if _, ok := grid[p]; ok {
+					parts[p] = append(parts[p], n)
+				}
+			}
+		}
+	}
+
+	part1, part2 := 0, 0
+	for p, ns := range parts {
+		prod := 1
+		for _, n := range ns {
+			part1 += n
+			prod *= n
+		}
+		if grid[p] == '*' && len(ns) == 2 {
+			part2 += prod
+		}
+	}
+	fmt.Println(part1)
+	fmt.Println(part2)
+}
